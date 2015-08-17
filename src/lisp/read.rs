72072c24652e80b0d499192d078a24f17fa5d147
patrikn <patrik.nordebo@atex.com>
2015-08-17 09:28:11 +0200

    Now with error handling

--- src/lisp/read.rs
+++ src/lisp/read.rs
@@ -1,84 +1,167 @@
 use std::iter::{Iterator,Peekable};
+use std::io;
+use std::num;
+use std::fmt;
 use std::error::{Error};
 pub use lisp::expr::{Add,Expression,Function,Call,Literal};
 
-pub fn repl<E: Error>(input: &mut Iterator<Item = Result<char, E>>) {
+#[derive(Debug)]
+pub enum ReadError {
+    Io(io::Error),
+    Invalid(String),
+    Parse(num::ParseIntError),
+    Eof
+}
+
+fn Invalid(message: &str) -> ReadError {
+    ReadError::Invalid(message.to_string())
+}
+
+impl From<io::Error> for ReadError {
+    fn from(err: io::Error) -> ReadError {
+        ReadError::Io(err)
+    }
+}
+
+impl From<num::ParseIntError> for ReadError {
+    fn from(err: num::ParseIntError) -> ReadError {
+        ReadError::Parse(err)
+    }
+}
+
+impl fmt::Display for ReadError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ReadError::Io(ref err) => write!(f, "IO error: {}", err),
+            ReadError::Parse(ref err) => write!(f, "Parse error: {}", err),
+            ReadError::Invalid(ref err) => write!(f, "Invalid input: {}", err),
+            ReadError::Eof => write!(f, "End of file")
+        }
+    }
+}
+
+
+pub fn repl(input: &mut Iterator<Item = Result<char, io::Error>>) {
+    let peekable = &mut input.peekable();
     loop {
-        let filtered: &mut Iterator<Item = char> = &mut input.map(|c| -> char {read_char(c)});
-        let peekable: &mut Peekable<&mut Iterator<Item = char>> = &mut filtered.peekable();
         let expr = read_expr(peekable);
-        let val = expr.eval();
-        println!("{}", val);
+        match expr {
+            Ok(expr) => println!("{}", expr.eval()),
+            Err(ReadError::Eof) => return,
+            Err(e) => println!("Error: {}", e)
+        }
     }
 }
 
-pub fn read_char<E: Error>(c: Result<char, E>) -> char {
-    c.ok().expect("IO error")
-}
-
-pub fn read_expr(input: &mut Peekable<&mut Iterator<Item = char>>)
-    -> Box<Expression>
+pub fn read_expr(input: &mut Peekable<&mut Iterator<Item = Result<char, io::Error>>>)
+    -> Result<Box<Expression>, ReadError>
 {
-    let c = *input.peek().expect("EOF while reading expr");
+    let c = match input.peek() {
+        Some(&Ok(ref c)) => Some(*c),
+        Some(&Err(ref e)) => None,
+        None => return Err(ReadError::Eof)
+    };
     match c {
-        '(' => {input.next();
-                return Box::new(Call::new(read_function_name(input),
-                                          read_function_params(input)))
-               },
-        '0'...'9'|'+'|'-' => Box::new(Literal::new(read_number(input))),
-        ' '|'\n'|'\r' => {input.next(); read_expr(input) },
-        _ => panic!("Invalid input '{}'", c)
+        Some(c) => match c {
+            '(' => {input.next();
+                    return Ok(Box::new(Call::new(try!(read_function_name(input)),
+                                                 try!(read_function_params(input)))))
+                   },
+            '0'...'9'|'+'|'-' => Ok(Box::new(Literal::new(try!(read_number(input))))),
+            ' '|'\n'|'\r' => {input.next(); Ok(try!(read_expr(input))) },
+            _ => { input.next(); Err(ReadError::Invalid(format!("Invalid input '{}'", c))) }
+        },
+        None => return Err(From::from(input.next().expect("Input disappeared!").err().expect("Error disappeared!")))
     }
 }
 
-pub fn read_function_name(input: &mut Peekable<&mut Iterator<Item = char>>) -> Box<Function> {
+pub fn read_function_name(input: &mut Peekable<&mut Iterator<Item = Result<char, io::Error>>>) -> Result<Box<Function>, ReadError> {
     let mut name = String::new();
     for c in input {
         match c {
-            ' ' => break,
-            _ => name.push(c)
+            Ok(' ') => break,
+            Ok(c) => name.push(c),
+            Err(e) => return Err(From::from(e))
         }
     }
 
     let n: &str = &name;
     match n {
-        "+" => return Box::new(Add::new()),
-        _ => panic!("Unknown function '{}'", name)
+        "+" => return Ok(Box::new(Add::new())),
+        _ => Err(ReadError::Invalid(format!("Unknown function '{}'", name)))
     }
 }
 
-pub fn read_function_params(input: &mut Peekable<&mut Iterator<Item = char>>) -> Vec<Box<Expression>> {
+macro_rules! try_peek {
+    ($expr:expr) => ({{let stupid_rust = {
+                          let peek = $expr.peek();
+                          match peek {
+                              Some(&Result::Ok(ref val)) => Some(*val),
+                              Some(&Result::Err(ref err)) => None,
+                              None => None
+                          }
+                      };
+                      match stupid_rust {
+                          Some(c) => Some(c),
+                          None => {
+                              match $expr.next() {
+                                  Some(Err(e)) => return Err(From::from(e)),
+                                  None => None,
+                                  Some(Ok(v)) => panic!("peek and next disagree")
+                              }
+                          }
+                      }
+                      }}
+                    )
+}
+
+pub fn read_function_params(input: &mut Peekable<&mut Iterator<Item = Result<char, io::Error>>>) -> Result<Vec<Box<Expression>>, ReadError> {
     let mut params: Vec<Box<Expression>> = vec![];
+    let mut acc = String::new();
     loop {
-        let c = *input.peek().expect("EOF while reading params");
+        // let stupid_rust = {
+        //     let peek = input.peek();
+        //     match peek {
+        //         Some(&Err(ref e)) => None,
+        //         Some(&Ok(ref c)) => Some(*c),
+        //         None => break
+        //     }
+        // };
+        // let c = match stupid_rust {
+        //     None => return Err(From::from(input.next().expect("Error disappeared").err().expect("Error disappeared!"))),
+        //     Some(c) => c
+        // };
+        let c = match try_peek!(input) {
+            Some(c) => c,
+            None => break
+        };
+        acc.push(c);
         println!("Reading param starting with {}", c);
         match c {
-            '0'...'9'|'-' => params.push(Box::new(Literal::new(read_number(input)))),
-            '(' => params.push(read_expr(input)),
+            '0'...'9'|'-' => params.push(Box::new(Literal::new(try!(read_number(input))))),
+            '(' => params.push(try!(read_expr(input))),
             ' ' => { input.next(); continue },
-            ')' => { input.next(); return params },
-            _ => panic!("Invalid input '{}'", c)
+            ')' => { input.next(); return Ok(params) },
+            _ => { input.next(); return Err(ReadError::Invalid(format!("Invalid input '{}'", c))) }
         }
     }
-    panic!("EOF while reading params");
+    Err(ReadError::Eof)
 }
 
-pub fn read_number(input: &mut Peekable<&mut Iterator<Item = char>>) -> i64 {
+pub fn read_number(input: &mut Peekable<&mut Iterator<Item = Result<char, io::Error>>>) -> Result<i64, ReadError> {
     let mut buf = String::new();
     loop {
-        let c = *input.peek().expect("EOF");
+        let c = try_peek!(input);
         match c {
-            '-'       => { buf.push(c); input.next(); if buf.len() > 1 { panic!("invalid number {}", buf); } },
-            '0'...'9' => { buf.push(c); input.next(); },
-            ' '       => break,
-            ')'       => break,
-            _         => panic!("Invalid input '{}'", c)
+            Some(c @ '-')       => { buf.push(c); input.next(); if buf.len() > 1 { return Err(ReadError::Invalid(format!("invalid number {}", buf))); } },
+            Some(c @ '0'...'9') => { buf.push(c); input.next(); },
+            Some(' ')       => break,
+            Some(')')       => break,
+            None            => { input.next(); return Err(ReadError::Eof) },
+            Some(c)         => { input.next(); return Err(ReadError::Invalid(format!("Invalid input '{}'", c))) }
         }
     }
-    match buf.parse() {
-        Ok(val) => return val,
-        Err(err) => { panic!("Not a number: {}", err) }
-    }
+    Ok(try!(buf.parse()))
 }
 
 
@@ -90,19 +173,22 @@ mod test {
 
     #[test]
     fn test_read_add_function() {
-        // Not sure how to tell what I get back but at least it doesn't panic
         let foo: &mut Iterator<Item = char> = &mut "+".chars();
         let peekable: &mut Peekable<&mut Iterator<Item = char>> = &mut foo.peekable();
-        read_function_name(peekable);
+        match read_function_name(peekable) {
+            Error(e) => panic!("Didn't get function"),
+            _ => ()
+        }
     }
 
     #[test]
-    #[should_panic(expected="Unknown function 'apa'")]
     fn test_read_unknown_function() {
-        // Not sure how to tell what I get back but at least it doesn't panic
         let foo: &mut Iterator<Item = char> = &mut "apa".chars();
         let peekable: &mut Peekable<&mut Iterator<Item = char>> = &mut foo.peekable();
-        read_function_name(peekable);
+        match read_function_name(peekable) {
+            Ok(e) => panic!("Should get error"),
+            _ => ()
+        }
     }
 
     #[test]
